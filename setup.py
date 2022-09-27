fb110de94f9a76fb3adf56466882e98c70c512e9
SridharPandian <sripan172@gmail.com>
2022-09-27 15:47:05 -0400

    Changed setup.py and extractor config.

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
     version='1.0.0',
     packages=find_packages(),
     description='Official implementation of the paper Holo-dex: Towards teaching dexterity with immersive Mixed Reality.',
-    # url='https://github.com/NYU-robot-learning/3D-Hand-Pose',
-    author='Sridhar Pandian Arunachalam, Irmak Guzey',
+    url='https://github.com/SridharPandian/Holo-Dex',
+    author='Sridhar Pandian Arunachalam, Irmak Guzey, Soumith Chintala, Lerrel Pinto',
 )
